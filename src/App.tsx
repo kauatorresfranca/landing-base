8a2af1fb9ce91205e981bbe3c7a637347bbe5991
kauatorresfranca <kauatorresfranca2@gmail.com>
2025-10-03 21:28:53 -0300

    setting components base

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AboutMe from "./components/sections/about-me"
+import FAQ from "./components/sections/faq"
+import Footer from "./components/sections/hero/footer"
+import Header from "./components/sections/hero/header"
+import Hero from "./components/sections/hero"
+import Services from "./components/sections/services"
+import Testimonials from "./components/sections/testemonials"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header />
+      <Hero />
+      <Services />
+      <AboutMe />
+      <Testimonials />
+      <FAQ />
+      <Footer />
+    </div>
   )
 }
 
